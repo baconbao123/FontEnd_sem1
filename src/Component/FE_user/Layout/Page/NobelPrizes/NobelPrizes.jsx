f927b2c89b642682d4aa9da4c3e1a5b929507ab1
baconbao123 <anh450460@gmail.com>
2023-06-16 19:58:32 +0700

    Long create Nobel prize

--- src/Component/FE_user/Layout/Page/NobelPrizes/NobelPrizes.jsx
+++ src/Component/FE_user/Layout/Page/NobelPrizes/NobelPrizes.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 import './NobelPrizes.css'
 function NobelPrizes() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className='container container-nobel'>
-      <h1 className='text-light ' style={{marginTop:'100px'}}>Nobel Pirze</h1>
+      <div className='mt-5' >
+      <Button variant="primary" onClick={handleShow} style={{marginTop:'200px'}}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
-  )
+
+    
+  );
 }
 
 export default NobelPrizes
