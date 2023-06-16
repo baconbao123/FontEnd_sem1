e3c7decc4992ac8643df3caa118e70aef4faefa5
baconbao123 <anh450460@gmail.com>
2023-06-16 16:14:42 +0700

    Long add <thư viện aos>, create Pages Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Routes, Route, Link } from "react-router-dom";
-
-import React, { useRef } from "react";
+import { Routes, Route ,useLocation } from "react-router-dom";
+import React from "react";
 import "./App.scss";
 
 import AD_home from "./Component/FE_admin/AD_component/AD_home";
@@ -9,22 +8,31 @@ import AD_home from "./Component/FE_admin/AD_component/AD_home";
 import Homepage from "./Component/FE_user/Layout/Page/Home/Homepage";
 import NobelPrizes from "./Component/FE_user/Layout/Page/NobelPrizes/NobelPrizes";
 import AlfredNobel from "./Component/FE_user/Layout/Page/AlfredNobel/AlfredNobel";
+import WillAlfedNobel from "./Component/FE_user/Layout/Page/WillAlfedNobel/WillAlfedNobel";
 import Navbar from "./Component/FE_user/Layout/Navbar";
+import Footer from "./Component/FE_user/Layout/Footer";
+import ScollToTop from "./Component/FE_user/Layout/ScrollToTop";
+
 function App() {
+  const location = useLocation();
+  
   return (
     <>
-      {/* amdin */}
+      {/* Admin */}
       <Routes>
         <Route path="/admin" element={<AD_home />} />
       </Routes>
-      {/* =======================user =========================*/}
-      {/* Navbar */}
-      <Navbar/>
+
+      {/* User */}
+      {location.pathname ==="/nobel-prizes" || location.pathname === "/alfred-nobel/nobel-will"  || location.pathname === "/"  || location.pathname==='/alfred-nobel' ? <Navbar /> : null}
+      
       <Routes>
-          <Route path="/" element ={<Homepage/>}/>
-          <Route path="/nobel-prizes" element ={<NobelPrizes/>}/>
-          <Route path="/alfred-nobel" element ={<AlfredNobel/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/nobel-prizes" element={<NobelPrizes />} />
+        <Route path="/alfred-nobel" element={<AlfredNobel />} />
+        <Route path="/alfred-nobel/nobel-will" element={<WillAlfedNobel />} />
       </Routes>
+      <ScollToTop />
     </>
   );
 }
