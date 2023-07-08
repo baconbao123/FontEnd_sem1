2f965ace2eb15b8242d71964d51bcc9886c34833
baconbao123 <anh450460@gmail.com>
2023-07-08 22:23:22 +0700

    Long up code

--- src/Component/FE_user/Layout/Page/NobelPrizes/NobelPrizes.jsx
+++ src/Component/FE_user/Layout/Page/NobelPrizes/NobelPrizes.jsx
@@ -19,8 +19,6 @@ function NobelPrizes() {
   const [nobelPrize, setNobelPrize] = useState([]);
   const { name, year, id } = useParams();
   const [imgIcon, setImg] = useState([]);
-
-  const imgBaseUrl="../NobelPrizes/img-icon";
   useEffect(() => {
     const data = async () => {
       const dataInfo = [
@@ -56,7 +54,7 @@ function NobelPrizes() {
   useEffect(() => {
     // simulate fetching data from an API
     const fetchData = async () => {
-      const res = await axios.get(`${URL}/api/personprizes/`);
+      const res = await axios.get(`${URL}/api/personprizes`);
       if (res && res.data) {
         setNobelPrize(res.data);
       }
@@ -92,123 +90,134 @@ function NobelPrizes() {
         prize.nobelPrize.length > 0
     );
 
-    return (
-      <div className="container nobel-prizes">
-        <section className="row col-lg-6 col-lg-6 title-nobel-prizes text-light">
-          <h1 data-aos="fade-up">All Nobel Prizes</h1>
-        </section>
-    
-        {/* Form select search */}
-        <section className="row mt-1 form-year-nobel-detail gap-3">
-          <Form.Select
-            className="form-select-nobel col-lg-12"
-            value={selectedPrize}
-            onChange={(e) => setSelectedPrize(e.target.value)}
-          >
-            <option className="active" value="Nobel Prizes">
-              Nobel Prizes
+  return (
+    <div className="container nobel-prizes">
+      <section className="row col-lg-6 col-lg-6 title-nobel-prizes text-light">
+        <h1 data-aos="fade-up">All Nobel Prizes</h1>
+      </section>
+
+      {/* Form select search */}
+      <section className="row mt-1 form-year-nobel-detail gap-3">
+        <Form.Select
+          className="form-select-nobel col-lg-12"
+          value={selectedPrize}
+          onChange={(e) => setSelectedPrize(e.target.value)}
+        >
+          <option className="active" value="Nobel Prizes">
+            Nobel Prizes
+          </option>
+          <option value="Physic Prize">Physics</option>
+          <option value="Chemistry Prize">Chemistry</option>
+          <option value="Literature Prize">Literature</option>
+          <option value="Medicine Prize">Medical</option>
+          <option value="Peace Prize">Peace</option>
+          <option value="Prize in Economic Sciences">Economic Sciences</option>
+        </Form.Select>
+        <Form.Select
+          className="form-select-nobel col-lg-12"
+          value={selectedYear}
+          onChange={(e) => setSelectedYear(e.target.value)}
+        >
+          <option className="active" value="Year">
+            All Year
+          </option>
+          {nobelPrize.map((prize, index) => (
+            <option value={prize.year} key={index}>
+              {prize.year}
             </option>
-            <option value="Physic Prize">Physics</option>
-            <option value="Chemistry Prize">Chemistry</option>
-            <option value="Literature Prize">Literature</option>
-            <option value="Medicine Prize">Medical</option>
-            <option value="Peace Prize">Peace</option>
-            <option value="Prize in Economic Sciences">Economic Sciences</option>
-          </Form.Select>
-          <Form.Select
-            className="form-select-nobel col-lg-12"
-            value={selectedYear}
-            onChange={(e) => setSelectedYear(e.target.value)}
+          ))}
+        </Form.Select>
+
+        <button
+          type="button"
+          className="btn btn-outline-primary btn-reset col-lg-12"
+          onClick={() => handleReset()}
+        >
+          Reset <BiRefresh />
+        </button>
+      </section>
+
+      {/* Item-nobel-prize */}
+      <section className="item-nobel-prizes w-100 mt-4 mb-4">
+        {filteredPrizes.length === 0 ? (
+          <div
+            style={{ color: "gray" }}
+            className="row justify-content-center col-lg-12 col-md-12"
           >
-            <option className="active" value="Year">
-              All Year
-            </option>
-            {nobelPrize.map((prize, index) => (
-              <option value={prize.year} key={index}>
-                {prize.year}
-              </option>
-            ))}
-          </Form.Select>
-    
-          <button
-            type="button"
-            className="btn btn-outline-primary btn-reset col-lg-12"
-            onClick={() => handleReset()}
-          >
-            Reset <BiRefresh />
-          </button>
-        </section>
-    
-        {/* Item-nobel-prize */}
-        <section className="item-nobel-prizes w-100 mt-4 mb-4">
-          {filteredPrizes.length === 0 ? (
-            <div
-              style={{ color: "gray" }}
-              className="row justify-content-center col-lg-12 col-md-12"
-            >
-              <img
-                src={require("../../../../img/no-data.png")}
-                alt=""
-                className="w-25"
-                style={{ filter: "grayscale(100%)" }}
-              />
-              <p className="text-center">
-                No Nobel Prizes found for the selected category.
-              </p>
-            </div>
-          ) : (
-            filteredPrizes.map((item, index) => {
-              if (item.status === "active") {
-                return (
-                  <React.Fragment key={index}>
-                    <div className="year text-light mt-4">
-                      <h3>{item.year}</h3>
-                    </div>
-                    {item.nobelPrize.map((prize, i) => {
-                       if (prize.status === "active") {
-                        const icon = imgIcon.find(
-                          (icon) => icon.id.toLowerCase() === prize.namePrize.toLowerCase()
-                        );
-                        const iconSrc = icon ? `${icon.imgicon}` : "";
-                        return (
-                          <Link
-                            to={`/nobel-prizes/${prize.namePrize}/${item.year}/${prize.id}`}
-                            key={`${index}-${i}`}
+            <img
+              src={require("../../../../img/no-data.png")}
+              alt=""
+              className="w-25"
+              style={{ filter: "grayscale(100%)" }}
+            />
+            <p className="text-center">
+              No Nobel Prizes found for the selected category.
+            </p>
+          </div>
+        ) : (
+          filteredPrizes.map((item, index) => {
+            if (item.status === "active") {
+              return (
+                <React.Fragment key={index}>
+                  <div className="year text-light mt-4">
+                    <h3>{item.year}</h3>
+                  </div>
+                  {item.nobelPrize.map((prize, i) => {
+                    if (prize.status === "active") {
+                      const icon = imgIcon.find(
+                        (icon) =>
+                          icon.id.toLowerCase() ===
+                          prize.namePrize.toLowerCase()
+                      );
+                      const iconSrc = icon ? `${icon.imgicon}` : "";
+                      return (
+                        <Link
+                          to={`/nobel-prizes/${prize.namePrize}/${item.year}/${prize.id}`}
+                          key={`${index}-${i}`}
+                        >
+                          <div
+                            className="item-nobel-prize col-lg-12 mt-4 d-flex"
+                            style={{ justifyContent: "space-between" }}
                           >
-                            <div
-                              className="item-nobel-prize col-lg-12 mt-4 d-flex"
-                              style={{ justifyContent: "space-between" }}
-                            >
-                              <div>
-                                <h3 className="title-nobel-prize mb-4">
-                                  The Nobel Prize in{" "}
-                                  {prize.namePrize
-                                    .replace("Prize", "")
-                                    .replace("in", "")}{" "}
-                                  {item.year}
-                                </h3>
-                                <p className="sub-nobel-prize">
-                                  <span>{prize.namePerson.join(", ")}:</span>{" "}
-                                  <span className="text-dark">{prize.motivation}</span>{" "}
-                                </p>
-                              </div>
-                              {icon && <img src={require(`../NobelPrizes/img-icon/${iconSrc}`)} width={100} height={110} alt="img-iocn"  className="rounded-circle"/>}
+                            <div>
+                              <h3 className="title-nobel-prize mb-4">
+                                The Nobel Prize in{" "}
+                                {prize.namePrize
+                                  .replace("Prize", "")
+                                  .replace("in", "")}{" "}
+                                {item.year}
+                              </h3>
+                              <p className="sub-nobel-prize">
+                                <span>{prize.namePerson.join(", ")}:</span>{" "}
+                                <span className="text-dark">
+                                  {prize.motivation}
+                                </span>{" "}
+                              </p>
                             </div>
-                          </Link>
-                        );
-                      }
-                      return null;
-                    })}
-                  </React.Fragment>
-                );
-              }
-              return null;
-            })
-          )}
-        </section>
-      </div>
-    );
-    
+                            {icon && (
+                              <img
+                                src={require(`../NobelPrizes/img-icon/${iconSrc}`)}
+                                width={100}
+                                height={110}
+                                alt="img-iocn"
+                                className="rounded-circle"
+                              />
+                            )}
+                          </div>
+                        </Link>
+                      );
+                    }
+                    return null;
+                  })}
+                </React.Fragment>
+              );
+            }
+            return null;
+          })
+        )}
+      </section>
+    </div>
+  );
 }
 
 export default NobelPrizes;
